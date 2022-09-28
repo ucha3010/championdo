bb4995693c1de85e4295d12b8ce934b4559553bf
Damian <damianjava@gmail.com>
2022-09-28 18:27:25 +0200

    Subida esqueleto

---
+++ src/main/java/com/championdo/torneo/TorneoApplication.java
@@ -0,0 +1,13 @@
+package com.championdo.torneo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorneoApplication.class, args);
+	}
+
+}
